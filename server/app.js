249b21cce7d629c045b670e0b6beb67e9d5d8614
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-07-29 09:57:15 +0100

    Add movie list homepage

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import Movie from '../models/Movie.js'
 
 const app = express()
 
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
 // TODO: add your endpoints here
+app.get('/', async (req, res) => {
+  const movies = await Movie.findAll()
+  res.render('index', { movies })
+})
 
 export default app
