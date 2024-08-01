d198122c592f172340d5affadaead3a0615a257a
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-08-01 09:47:51 +0100

    Do exercise d2e1

--- exercises/d2e1.js
+++ exercises/d2e1.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs')
 app.set('views', 'exercises/views')
 
 app.get('/d2e1', (req, res) => {
-  // TODO: Render 'd2e1.ejs'
+  res.render('d2e1')
 })
 
 // TODO: Open d2e1.ejs and follow the instructions
