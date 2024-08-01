20abcc2587d2992cadf179c7dabca19dfcdf7265
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2024-08-01 11:02:27 +0100

    Add sort dropdown to index page

--- server/app.js
+++ server/app.js
@@ -10,13 +10,13 @@ app.set('view engine', 'ejs')
 // TODO: add your endpoints here
 app.get('/', async (req, res) => {
   let movies
-  if (req.query.sort === "asc") {
+  if (req.query.sort === "dateAsc") {
     movies = await Movie.findAll(undefined, true)
   } else {
     movies = await Movie.findAll()
   }
   
-  res.render('index', { movies })
+  res.render('index', { movies, sort: req.query.sort })
 })
 
 app.get('/movie/:movieId', async (req, res) => {
